c9e66896e3ab39d02b179d5e237f3b50da80cb09
Dyujoy <dyujoy1234@gmail.com>
2022-01-04 20:50:52 +0800

    Added login

--- server/app.js
+++ server/app.js
@@ -6,13 +6,17 @@ const port = process.env.PORT || 8000;
 
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control_Allow-Headers', "x-access token, Origin, Content-Type, Accept");
     res.setHeader("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTYzNjQ5MTkyNzA5MiIsImVtYWlsIjoiY0BjIiwiaWF0IjoxNjM2NDkxOTI3LCJleHAiOjE2MzY0OTkxMjd9.3nKmcqEh9NSx8qLX-OaVjOqIeTSBFk4BjQxELrQf1O4");
     next();
 })
+
 app.use(require('./routes'));
+
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 
+const auth = require('../components/auth');
+
 router.get('/', async (req, res, next) => {
     return res.json("Hello World");
 })
+
+router.post('/login', async (req, res, next) => {
+    auth.authenticate(req.body)
+        .then(user=>{
+            if(user){
+                return res.json(user)
+            }
+            return res.status(400).json({message: 'Username or password incorrect'});
+        })
+        .catch(e=>{
+            return next(e)
+        })
+})
+
 
 module.exports = router;
